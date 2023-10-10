9d62e82a74ff3dde4d33ceff60096142bf00b617
akt13 <abhijithkthoppil@gmail.com>
2023-10-10 20:46:59 +0530

    tabs added

--- src/components/layout.py
+++ src/components/layout.py
@@ -1,14 +1,25 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output, callback
 from . import expense
 
+
 def create_layout(app: Dash) -> html.Div:
-    return html.Div(
-        className="app-div",
-        children=[
-            html.H1(app.title),
-            html.Div(
-                className = "text_Box",
-                children = [expense.render(app)]
-            )             
-        ]
-    )
+    return html.Div([
+        dcc.Tabs([
+            dcc.Tab(label='Overview', children=[
+                html.H6("Page with overview infos")
+            ]),
+            dcc.Tab(label='Investment Summary', children=[
+                html.H6(
+                    "Page with details of NPS, PPF, MF, Stocks, SGB etc details ")
+            ]),
+            dcc.Tab(label='Stocks', children=[
+                html.H6("Page with detailed stocks infos")
+            ]),
+            dcc.Tab(label='Expense Tracker', children=[
+                expense.render(app)
+            ]),
+            dcc.Tab(label='Taxes', children=[
+                html.H6("Page with income tax info")
+            ]),
+        ]),
+    ])
